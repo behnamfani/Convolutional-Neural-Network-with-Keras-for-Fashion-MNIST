a198685e1dd2b01f72c8de0d1ccce693bff8ccef
Behnam Fani <b.fanitabasi@hotmail>
2020-01-11 03:36:50 +0330

    First

---
+++ main.py
@@ -0,0 +1,50 @@
+import keras
+import numpy as np
+
+keras.backend.set_image_data_format('channels_first')
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+print(x_test.shape,' ', x_train.shape)
+print("x_train shape:", x_train.shape, "y_train shape:", y_train.shape)
+
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+(x_train, x_valid) = x_train[5000:], x_train[:5000]
+(y_train, y_valid) = y_train[5000:], y_train[:5000]
+
+
+w, h = 28, 28
+x_train = x_train.reshape(x_train.shape[0], 1, w, h)
+x_valid = x_valid.reshape(x_valid.shape[0], 1, w, h)
+x_test = x_test.reshape(x_test.shape[0], 1, w, h)
+y_train = keras.utils.to_categorical(y_train, 10)
+y_valid = keras.utils.to_categorical(y_valid, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu', data_format='channels_first',
+                              input_shape=(1, 28, 28)))
+model.add(keras.layers.MaxPooling2D(pool_size=2, data_format='channels_first'))
+model.add(keras.layers.Dropout(0.3))
+model.add(
+    keras.layers.Conv2D(filters=32, kernel_size=2, padding='same', activation='relu', data_format='channels_first'))
+model.add(keras.layers.MaxPooling2D(pool_size=2, data_format='channels_first'))
+model.add(keras.layers.Dropout(0.3))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(256, activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+checkpointer = keras.callbacks.ModelCheckpoint(filepath='model.weights.best.hdf5', verbose=1, save_best_only=True)
+model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_valid, y_valid), callbacks=[checkpointer])
+
+model.load_weights('model.weights.best.hdf5')
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print('\n', 'Test accuracy:', score[1])

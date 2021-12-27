6414db910034981ceadf1c003e3a2be38ce73b82
behnam fani <behnam.fani18@gmail.com>
2021-12-28 00:43:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from keras.datasets import fashion_mnist
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 keras.backend.set_image_data_format('channels_first')
 
+# Reading the data and dividing it to train, validation, and test sets.
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-print(x_test.shape,' ', x_train.shape)
-print("x_train shape:", x_train.shape, "y_train shape:", y_train.shape)
+
+print("x_train shape:", x_train.shape, " y_train shape:", y_train.shape)
+print("x_test shape:", x_test.shape, " y_test shape:", y_test.shape)
 
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
@@ -23,6 +26,7 @@ y_train = keras.utils.to_categorical(y_train, 10)
 y_valid = keras.utils.to_categorical(y_valid, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
+# Defining and constructing the model
 model = keras.Sequential()
 model.add(keras.layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu', data_format='channels_first',
                               input_shape=(1, 28, 28)))
@@ -41,11 +45,28 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Fitting the model on train and validation sets and saving the weights
 checkpointer = keras.callbacks.ModelCheckpoint(filepath='model.weights.best.hdf5', verbose=1, save_best_only=True)
-model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_valid, y_valid), callbacks=[checkpointer])
+history = model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_valid, y_valid), callbacks=[checkpointer])
 
+# Plotting history of model's accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'])
+plt.show()
+# Plotting history of model's loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model (categorical_crossentropy) Loss')
+plt.ylabel('Loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'])
+plt.show()
+
+# Checking the model performance on test data 
 model.load_weights('model.weights.best.hdf5')
-
 score = model.evaluate(x_test, y_test, verbose=0)
-
 print('\n', 'Test accuracy:', score[1])
